b473e806553521bcdd4a8bc5fdd5ae5a9646f0ba
mamicof <112802183+mamicof@users.noreply.github.com>
2025-04-13 22:36:08 +0900

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<!-- src/routes/+layout.svelte -->
+<script>
+  export let data;
+</script>
+
+<svelte:head>
+  <!-- 検索避け -->
+  <meta name="robots" content="noindex, nofollow" />
+
+  <!-- OGP無効化 -->
+  <meta property="og:title" content="no-title" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="" />
+  <meta property="og:description" content="This page is private." />
+  <meta name="twitter:card" content="none" />
+</svelte:head>
+
+<slot />
