e5e4c121335a0abcf8686ab081e0a1eee4ddd3e5
mamicof <112802183+mamicof@users.noreply.github.com>
2025-04-13 19:47:28 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-  import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
-  export default {
-    kit: {
-      adapter: adapter()
-    }
-  };
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
