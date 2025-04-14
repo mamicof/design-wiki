4ae219df96834821c5ddb028e15f0d1f6ddb7635
mamicof <112802183+mamicof@users.noreply.github.com>
2025-04-14 17:15:56 +0900

    Create +page.js

---
+++ src/routes/+page.js
@@ -0,0 +1,13 @@
+// src/routes/+page.js
+import { getArticles } from '$lib/utils/article';
+
+export async function load() {
+  const allArticles = await getArticles();
+  const popularArticles = allArticles.slice(0, 4); // 仮の人気順
+  const recentArticles = allArticles.slice(-4).reverse(); // 仮の新着順
+
+  return {
+    popularArticles,
+    recentArticles
+  };
+}
