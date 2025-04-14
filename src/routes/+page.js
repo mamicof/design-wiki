d1342bbe04ee85b061d88284cd2eb01d3690c480
mamicof <112802183+mamicof@users.noreply.github.com>
2025-04-14 17:43:22 +0900

    Update +page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,10 @@
 // src/routes/+page.js
-import { getArticles } from '$lib/utils/article';
+import { getArticles } from '$lib/utils/articleUtils';
 
 export async function load() {
   const allArticles = await getArticles();
-  const popularArticles = allArticles.slice(0, 4); // 仮の人気順
-  const recentArticles = allArticles.slice(-4).reverse(); // 仮の新着順
+  const popularArticles = allArticles.slice(0, 4);
+  const recentArticles = allArticles.slice(-4).reverse();
 
   return {
     popularArticles,
