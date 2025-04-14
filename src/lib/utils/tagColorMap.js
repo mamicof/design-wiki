acc314203c9f5eb9ef1873586681ff19b7238182
mamicof <112802183+mamicof@users.noreply.github.com>
2025-04-14 23:08:34 +0900

    Create tagColorMap.js

---
+++ src/lib/utils/tagColorMap.js
@@ -0,0 +1,16 @@
+// src/lib/utils/tagColorMap.js
+
+export const tagColorMap = {
+  色: 'tag--yellow',
+  入力: 'tag--teal',
+  フォーム: 'tag--blue',
+  リキャプチャ: 'tag--red',
+  SEO: 'tag--green',
+  UIパターン: 'tag--orange',
+  表記: 'tag--pink',
+  コンポーネント: 'tag--gray',
+  パンくず: 'tag--brown',
+  通知: 'tag--purple',
+  UX: 'tag--blue',
+  // 必要に応じてここに追加
+};
